c76c9b74812f90241739a0f98e1afb7cba1cf2c1
Pedro101520 <pedrolimazoi10@gmail.com>
2024-04-25 10:43:32 -0300

    Criação de conta

--- SistemaBancario/criacaoDeConta/validaConta.py
+++ SistemaBancario/criacaoDeConta/validaConta.py
@@ -1,12 +1,18 @@
 from ..cadastroDeUsuario.armazenaUsuario import getCliente
 
+from tkinter import messagebox
+
 def criaConta(cpf):
     dadosCliente = getCliente()
+    cliente_encontrado = False
     for cliente in dadosCliente:
         if cpf == cliente["CPF"]:
-            cliente["Agencia"] = "0001"
-            cliente["Conta"] = 1
-        else:
-            print("Conta não encontrada")
-    for cliente in dadosCliente:
-        print(cliente)
+            cliente_encontrado = True
+            if "Contas" not in cliente:
+                cliente["Contas"] = []
+            nova_conta = {"Agencia": "0001", "Numero": len(cliente["Contas"]) + 1}
+            cliente["Contas"].append(nova_conta)
+            print(cliente)
+            break
+    if not cliente_encontrado:
+        messagebox.showinfo("Atenção", "Usuário não cadastrado")
